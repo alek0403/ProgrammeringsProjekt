0d40a455e905e589b2d71a24f28e1fad099627c2
alek0403 <88056694+alek0403@users.noreply.github.com>
2023-06-12 13:10:53 +0200

    Add files via upload

--- BaseProject/Inc/timing_config.h
+++ BaseProject/Inc/timing_config.h
@@ -2,7 +2,18 @@
 #define TIMING_CONFIG_H_
 
 timersetup();
-TIM1_BRK_TIM15_IRQHandler(void);
+// laver struktur som har tid og et flag.
+struct tick {
+    volatile int32_t h;
+    volatile int32_t m;
+    volatile int32_t s;
+    volatile int32_t hs;
+    volatile int8_t flag;
+};
 
+
+
+
+struct tick ticks; // Laver instans af tick.
 
 #endif /* TIMING_CONFIG_H_ */

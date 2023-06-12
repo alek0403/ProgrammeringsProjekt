436e856152754efe6b10a3fdb3a169438e654e9b
Jesperssovseskaal <136309760+Jesperssovseskaal@users.noreply.github.com>
2023-06-12 11:25:44 +0200

    Add files via upload

---
+++ Src/main.c
@@ -0,0 +1,52 @@
+#include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h" // Input/output library for this course
+#include "ansi.h"
+#include "Lut1.h"
+
+
+int main(void){
+	uart_init(9600);
+
+	Bane();
+
+	while(1){
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

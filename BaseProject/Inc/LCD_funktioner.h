815fac1d44bd37160ad9cbaa503c5d8630d02390
alek0403 <88056694+alek0403@users.noreply.github.com>
2023-06-12 15:27:30 +0200

    Add files via upload

--- BaseProject/Inc/LCD_funktioner.h
+++ BaseProject/Inc/LCD_funktioner.h
@@ -5,8 +5,10 @@
 
 lcd_write_string(char* string, uint8_t line, uint8_t* graphics_buffer);
 lcd_update(char* string);
-void lcd_scroll_text(const char* text, uint8_t* graphics_buffer, uint8_t scroll_interval);
-
+lcd_scroll_text(const char* text, uint8_t* graphics_buffer, uint8_t scroll_interval);
+LCD_liv(uint8_t *liv, uint8_t* graphics_buffer);
+LCD_LVL(uint8_t *lvl, uint8_t* graphics_buffer);
+LCD_Score(uint8_t *score, uint8_t* graphics_buffer);
 
 
 #endif /* LCD_FUNKTIONER_H_ */
